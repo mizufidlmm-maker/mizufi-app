ad9bf934621171077157e12e349c513f9517dee4
Mizu Finance <mizufidlmm@gmail.com>
2025-12-30 19:53:36 +0700

    Create wallet-button.tsx

---
+++ components/wallet-button.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export function WalletButton() {
+  return (
+    <button className="rounded-xl bg-ocean-500/20 px-4 py-2 text-sm ring-1 ring-ocean-300/20 hover:bg-ocean-500/30">
+      Connect Wallet
+    </button>
+  )
+}
